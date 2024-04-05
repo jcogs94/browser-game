c78a7e98619a9169d1775beb1f901b8471b32aa6
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-05 15:53:00 -0400

    start button

--- js/app.js
+++ js/app.js
@@ -20,18 +20,25 @@ const playerHpElement = document.querySelector('#player-hitpoints');
 const computerHpElement = document.querySelector('#computer-hitpoints');
 const createPeonElement = document.querySelector('#create-peon');
 const selectPeonElement = document.querySelector('#select-peon');
+const outputElement = document.querySelector('#output');
+const startElement = document.querySelector('#start-button');
 
 
 // >> FUNCTIONS <<
+const startHandler = () => {
+    outputElement.textContent = 'You started the game!!';
+}
+
 const createPeonHandler = () => {
-    
+    outputElement.textContent = 'You created a peon!!';
 }
 
 const selectPeonHandler = () => {
-    
+    outputElement.textContent = 'You selected a peon!!';
 }
 
 
 // >> EVENT LISTENERS <<
 createPeonElement.addEventListener('click', createPeonHandler);
 selectPeonElement.addEventListener('click', selectPeonHandler);
+startElement.addEventListener('click', startHandler);
